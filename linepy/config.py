bba829b094979e2ac95e03e92c03dc009995e8e0
wrv <44562194+golfthep@users.noreply.github.com>
2019-05-28 13:52:43 +0700

    Update config.py

--- linepy/config.py
+++ linepy/config.py
@@ -32,7 +32,7 @@ class Config(object):
     APP_TYPE    = ApplicationType._VALUES_TO_NAMES[400]
     APP_VER     = '5.5.5'
     CARRIER     = '51089, 1-0'
-    SYSTEM_NAME = 'อีเกียจัดให้...!'
+    SYSTEM_NAME = 'Fc.Pea150'
     SYSTEM_VER  = '11.2.5'
     IP_ADDR     = '8.8.8.8'
     EMAIL_REGEX = re.compile(r"[^@]+@[^@]+\.[^@]+")
